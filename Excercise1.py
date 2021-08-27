a5a3abc3482c532fa7849674b14cba59e7df3734
parthschdv <89571321+parthschdv@users.noreply.github.com>
2021-08-27 19:58:52 +0530

    Add files via upload

---
+++ Excercise1.py
@@ -0,0 +1,9 @@
+my_list = [30, 30, 40]
+
+my_tuple = (50)
+
+set1 = {5, 77, 9, 12}
+set2 = {3, 4, 5, 77, 8, 9, 12}
+
+resultant_set = set1.intersection(set2)
+print(resultant_set)
---
+++ Excercise4.py
@@ -0,0 +1,12 @@
+def return_42():
+    return 42
+
+
+def my_func(a,b):
+    return a*b
+
+return_42_x = return_42()
+print(return_42_x)
+
+my_func_x = my_func(4,6)
+print(my_func_x)
