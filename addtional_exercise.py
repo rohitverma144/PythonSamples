d78eb71b8716d6d6122d5ee94396132eee61778a
IamYougal <89571710+IamYougal@users.noreply.github.com>
2021-09-01 15:30:45 +0530

    Update addtional_exercise.py

--- addtional_exercise.py
+++ addtional_exercise.py
@@ -1,17 +1,18 @@
 import math
 
 def is_perfect_root(b):
-    if(b >= 0):
-        sr = math.sqrt(b)
-        return ((sr*sr) == float(b))
-    return False
+        sqr = math.sqrt(b)
+        if((sqr*sqr) == float(b)):
+            return True
+        else:
+            return False
 
 def find_true(c):
-    for i in range(1,int(math.sqrt(c)))):
+    for i in range(1,int(math.sqrt(c))):
         b = c - i*i 
         if is_perfect_root(b) and b > 0:
             return True
     return False
 
 
-print(find_true(3))
+print(find_true(5))
