9b76dd91f7552205a7928f7460fef542268e5e58
IamYougal <89571710+IamYougal@users.noreply.github.com>
2021-09-01 14:51:45 +0530

    Add files via upload

---
+++ addtional_exercise.py
@@ -0,0 +1,17 @@
+import math
+
+def is_perfect_root(b):
+    if(b >= 0):
+        sr = math.sqrt(b)
+        return ((sr*sr) == float(b))
+    return False
+
+def find_true(c):
+    for i in range(1,c):
+        b = c - i*i 
+        if is_perfect_root(b) and b > 0:
+            return True
+    return False
+
+
+print(find_true(3))
